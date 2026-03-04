296719d54cf4de02b12a0eee53433bc2bd51fcba
romiebrock-code <romiebrock@gmail.com>
2026-03-04 19:30:59 +0200

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE = "fun-paeds-v3";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k))))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
