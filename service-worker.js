595754f854b7715ec37bb46eb91ec29b7dda2968
romiebrock-code <romiebrock@gmail.com>
2026-03-04 23:22:05 +0200

    Version 9

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "fun-paeds-v8";
+const CACHE = "fun-paeds-v9";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
